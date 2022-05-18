b74c0ab5449077da21557a1143a1e14000e30796
bhawna23999 <bhavnaagrawal1999@gmail.com>
2022-05-18 23:56:37 +0530

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,4 +1,6 @@
-const urlModel = require("../model/urlModel")
+const urlModel = require("../model/urlModel");
+const shortid = require("shortid")
+const redis = require("redis");
 
 
 const isValid = function(value){
@@ -12,55 +14,119 @@ const isValidRequestBody = function(requestBody){
 }
 
 
+const { promisify } = require("util")
+
+//Connect to redis
+const redisClient = redis.createClient(
+    10982,
+    "redis-10982.c246.us-east-1-4.ec2.cloud.redislabs.com",
+    { no_ready_check: true}
+);
+redisClient.auth("dGdtGmV1L3V2lEW8itMmqlPLuVWFr4Et", function (err){
+    if (err) throw err;
+})
+
+redisClient.on("connect", async function(){
+    console.log("Connected to Redis..")
+})
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
+
+
 const shorten = async function(req,res){
 
-    let requestBody = req.body
+    try
+    {
+        let requestBody = req.body
 
-    if(!(isValidRequestBody)){
-        return res.status(400).send({status:false, message:'Please enter Details'})
+        if(!(isValidRequestBody)){
+            return res.status(400).send({status:false, message:'Please enter Details'})
+        }
+
+        // Extract Param
+        const {longUrl} = requestBody 
+
+        if(!isValid(longUrl)){
+            return res.status(400).send({status:false, message:'Long-Url is Required'})
+        }
+
+        if(!/^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm.test(longUrl)){
+            return res.status(400).send({status:false, message:'Plese enter valid Url'})
+        }
+
+        const findUrl = await urlModel.findOne({longUrl:longUrl})
+
+        if(findUrl){
+            return res.status(200).send({status:false, message:"This Url is already Registered"})
+        }
+
+        let baseUrl = 'http://localhost:3000'
+
+        if(!/^(ftp|http|https):\/\/[^ "]+$/.test(baseUrl)){
+            return res.status(400).send({status:false, message:"Please send valid base url"})
+        }
+
+        let urlCode = shortid.generate()
+
+        let shortUrl = baseUrl + "/" + urlCode
+
+        requestBody.urlCode = urlCode
+
+        requestBody.shortUrl = shortUrl
+ 
+        await urlModel.create(requestBody)
+
+        let urlData = await urlModel.findOne({urlCode:urlCode}).select("longUrl shortUrl urlCode")
+        res.status(201).send({status:true, data:urlData})
+
+    }
+    catch(err)
+    {
+        console.log(err.message)
+        res.status(500).send({status:false, Error:err.message})
     }
 
-    //Extract Params
-    const {urlCode, longUrl, shortUrl} = requestBody 
-
-    if(!isValid(urlCode)){
-        return res.status(400).send({status:false, message:'Url-Code is Required'})
-    }
-
-    if(!isValid(longUrl)){
-        return res.status(400).send({status:false, message:'Long-Url is Required'})
-    }
-
-    if(!/^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm.test(longUrl)){
-        return res.status(400).send({status:false, message:'Plese enter valid Url'})
-    }
-
-    if(!isValid(shortUrl)){
-        return res.status(400).send({status:false, message:'Short-Url id required'})
-    }
-
-    let urlCreated = await urlModel.create(requestBody)
-    res.status(201).send({status:true, data:urlCreated})
-
+    
 }
-
+    
 
 const getUrlCode = async function(req,res){
 
-    const paramsId = req.params.urlCode
+    try
+    {
+        let urlCode = req.params.urlCode
 
-    if(!isValid(paramsId)){
-        return res.status(400).send({status:false, message:"Please enetr valid Url-Code"})
+        let cachedUrlData = await GET_ASYNC(`${urlCode}`)
+    
+        if(cachedUrlData){
+            return res.status(302).redirect(JSON.parse(cachedUrlData))
+        }
+    
+        let findUrl = await urlModel.findOne({urlCode:urlCode}) 
+    
+        if(!findUrl){
+            return res.status(404).send({status:false, message:`This ${req.params.urlCode} Url Code is not found.`})
+        }
+    
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(findUrl.longUrl))
+        res.status(200).redirect( findUrl.longUrl)
+
     }
+    catch(err)
+    {
+        console.log(err.message)
+        res.status(500).send({status:false, Error:err.message})
 
-    let findUrl = await urlModel.findOne({urlCode:paramsId})
-
-    if(!findUrl){
-        return res.status(404).send({status:false, message:`This ${paramsId} Url Code is not found.`})
-    }
-
-    res.status(200).send({status:true, data:findUrl})
+    } 
 }
+
+
 
 
 
--- src/model/urlModel.js
+++ src/model/urlModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 
 const urlSchema = new mongoose.Schema({
 
-    longUrl : {type:String, required:true, trim:true},
-    shortUrl : {type:String, required:true, unique:true, trim:true},
+    longUrl : {type:String, required:true, trim:true, lowercase:true},
+    shortUrl : {type:String, required:true, unique:true, trim:true, lowercase:true},
     urlCode : { type:String, unique:true, required:true, lowercase:true, trim:true},
 
 
