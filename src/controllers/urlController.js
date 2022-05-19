df201073fcc1b6d5c0bb5f34344a51e886b48c7e
bhawna23999 <bhavnaagrawal1999@gmail.com>
2022-05-19 18:59:30 +0530

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,18 +2,6 @@ const urlModel = require("../model/urlModel");
 const shortid = require("shortid")
 const redis = require("redis");
 
-
-// const isValid = function(value){
-//     if(typeof value === 'undefined' || value === null) return false
-//     if(typeof value === 'string' && value.trim().length === 0) return false
-//     return true
-// }
-
-// const isValidRequestBody = function(requestBody){
-//     return Object.keys(requestBody).length > 0
-// }
-
-
 const { promisify } = require("util")
 
 //Connect to redis
